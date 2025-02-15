77b9c63ee50a8323c4b64728d57267ed106ba921
jaky-sheikh <jaky@eu4m.eu>
2025-02-15 12:42:56 +0600

    navbar complete

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,19 +1,21 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 
 const MainLayout = () => {
     return (
-        <div>
+        <div className="w-11/12 mx-auto">
             <header>
                 <nav>
-
+                    <Navbar></Navbar>
                 </nav>
             </header>
             <main>
                 <Outlet></Outlet>
             </main>
             <footer>
-
+                <Footer></Footer>
             </footer>
         </div>
     );
---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,7 @@
+
+
+const AuthProvider = () => {
+    return
+};
+
+export default AuthProvider;
