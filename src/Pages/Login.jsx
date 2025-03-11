835602b1237f3a78c58435788de9ed9b64bef1b5
jaky-sheikh <jaky@eu4m.eu>
2025-03-11 21:25:23 +0600

    signin with lottie

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,38 @@
+import Lottie from 'lottie-react';
 import React from 'react';
+import signInLottieData from '../assets/lottie/signin.json';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <h2>Hello Login</h2>
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <Lottie animationData={signInLottieData}></Lottie>
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <h1 className="text-3xl font-bold text-center">Login now!</h1>
+                    <form className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="password" className="input input-bordered" required />
+
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                    </form>
+                    <h3 className="text-center pb-2">Dont have an account? Please <Link className="text-orange-500" to="/auth/register">Register</Link></h3>
+                </div>
+            </div>
         </div>
     );
 };
