f6d66a2acbec5c2b734625f933e5bc5e3df2870d
jaky-sheikh <jaky@eu4m.eu>
2025-03-21 11:33:25 +0600

    modified code

--- src/components/Guaranteed.jsx
+++ src/components/Guaranteed.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Guaranteed = () => {
     return (
-        <div className='text-center bg-cyan-500 h-auto rounded-sm my-8 py-4'>
+        <div className='text-center bg-cyan-500 h-auto lg:h-40 rounded-sm my-8 py-4'>
             <h2 className='text-5xl font-bold'>Lessons you’ll love. Guaranteed.</h2>
-            <p className='text-lg'>Try another tutor for free if you’re not satisfied.</p>
+            <p className='text-lg lg:pt-5'>Try another tutor for free if you’re not satisfied.</p>
         </div>
     );
 };
